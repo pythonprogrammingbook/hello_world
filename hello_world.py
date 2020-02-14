b26a5faf0e4cf1acb8d260fa94d1eac1ba5f9a41
alex <43364498@qq.com>
2020-02-14 19:39:28 +0800

    创建hello_world.py和tests.py

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+def get_info(source):
+    return "hello world from " + source
+
+if __name__ == "__main__":
+    print(get_info("Alex"))
+    
---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest  #导入unittest模块
+from hello_world import get_info
+#创建一个继承unittest.TestCase的类
+class Tests(unittest.TestCase):
+    def test_get_info(self):
+        self.assertEqual(
+            get_info('Alex'),
+            'hello world from Alex')
+
+if __name__ == '__main__':
+    unittest.main()
+    
